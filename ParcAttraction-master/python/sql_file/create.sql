4575316c0220a785272643e2bd68e4b03f988359
Jules <jules.gouerec@gmail.com>
2024-03-08 17:30:39 +0100

    ajout des critiques en sql

--- ParcAttraction-master/python/sql_file/create.sql
+++ ParcAttraction-master/python/sql_file/create.sql
@@ -2,3 +2,6 @@ INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Silver S
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Montagne 8', 'Montagne russe', 4, 1);
 
 INSERT INTO users (name, password) VALUES ('toto', 'toto');
+
+INSERT INTO critique (nom, prenom, message, note, attraction_id) VALUES ('Brice', 'de Nice', 'Super', 5, 1);
+INSERT INTO critique (nom, prenom, message, note, attraction_id) VALUES ('Anonyme', '', 'Nul', 1, 2);
--- ParcAttraction-master/python/sql_file/init.sql
+++ ParcAttraction-master/python/sql_file/init.sql
@@ -23,6 +23,8 @@ DROP TABLE IF EXISTS critique;
 CREATE TABLE critique (
     critique_id int auto_increment,
     primary key(critique_id),
+    nom varchar(255),
+    prenom varchar(255),
     message varchar(255),
     note int not null,
     attraction_id int not null
